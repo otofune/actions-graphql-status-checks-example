77c9296e66f194374c3cd872c2a1bd75019971c5
otofune <otofune@otofune.me>
2022-12-07 23:32:36 +0900

    exclude

--- .github/workflows/graphql.js
+++ .github/workflows/graphql.js
@@ -35,20 +35,94 @@ const query = `query commitRef($owner: String!, $repo: String!, $expression: Str
 }
 `
 
+const statusFromCheckRollupContextConnection = (c) => {
+  switch (c.__typename) {
+    case "CheckRun": {
+      if (c.status !== "COMPLETED") {
+        return "PENDING"
+      }
+      switch (c.conclusion) {
+        case "SUCCESS": {
+          return "SUCCESS"
+        }
+        case "SKIPPED": {
+          console.log(`ignore skipped run: ${c.permalink}`)
+          return "IGNORE"
+        }
+        case "NEUTRAL": {
+          console.log(`ignore neutral run: ${c.permalink}`)
+          return "IGNORE"
+        }
+        default: {
+          return "FAILURE"
+        }
+      }
+    }
+    case "StatusContext": {
+      switch (c.state) {
+        case "SUCCESS": {
+          return "SUCCESS"
+        }
+        case "PENDING": {
+          return "PENDING"
+        }
+        case "EXPECTED": {
+          console.log(`TODO: unknown state "EXPECTED" given: ${c}`)
+        }
+        default: {
+          return "FAILURE"
+        }
+      }
+    }
+    default: {
+      throw new Error("unexpected type")
+    }
+  }
+}
+
 module.exports = async ({ context, github }) => {
-  // get with ref (will break with tag ref) 
-  {
-    console.log(context.ref)
-    const res = await github.graphql(query, { ...context.repo, expression: context.ref })
-    console.log(JSON.stringify(res, null, "\t"))
+  // get with sha
+  const res = await github.graphql(query, { ...context.repo, expression: context.sha })
+  console.log(JSON.stringify(res, null, "\t"))
+
+  if (!res.repository.object) throw new Error("object is null")
+  if (res.repository.object.__typename !== "Commit") throw new Error("Object is not commit: " + res.repository.object.__typename)
+
+  const { statusCheckRollup: { contexts }} = res.repository.object
+  const filteredContext = contexts.filter(c => {
+    if (c.__typename === "CheckRun" && c.permalink.includes(context.runId.toString(10))) {
+      console.log("exclude self context")
+      console.dir(c)
+      return false
+    }
+    return true
+  })
+
+  let isPending = false
+  let isFailure = false
+  filteredContext.forEach(c => {
+    const st = statusFromCheckRollupContextConnection(c)
+    switch (st) {
+      case "IGNORE":
+        break
+      case "FAILURE":
+        isFailure = true
+        return
+      case "PENDING":
+        isPending = true
+      case "SUCCESS":
+        break
+    }
+  })
+  let conclusion = "SUCCESS"
+  if (isPending) {
+    conclusion = "PENDING"
+  }
+  if (isFailure) {
+    conclusion = "FAILURE"
   }
 
-  // get with sha
-  {
-    console.log(context.sha)
-    const res = await github.graphql(query, { ...context.repo, expression: context.sha })
-    console.log(JSON.stringify(res, null, "\t"))
-  }
+  console.log(`conclusion = ${conclusion}`)
 
   return
 }
