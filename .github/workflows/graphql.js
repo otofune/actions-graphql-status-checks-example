c2716df6e8c2f3503b06e15caa302630309e6054
otofune <otofune@otofune.me>
2022-12-07 20:47:53 +0900

    implement graphql

--- .github/workflows/graphql.js
+++ .github/workflows/graphql.js
@@ -1,3 +1,52 @@
-module.exports = async ({ context, github }) => {
-    return // WIP
+// to improve implement paging
+const query = `query commitRef($owner: String!, $repo: String!, $expression: String!) {
+  repository(name: $repo, owner: $owner) {
+    object(expression: $expression) {
+      __typename
+      ... on Commit {
+        statusCheckRollup {
+          state
+          contexts(first: 100) {
+            totalCount
+            nodes {
+              __typename
+              ... on CheckRun {
+                completedAt
+                conclusion
+                permalink
+                startedAt
+                status
+                summary
+                text
+              }
+              ... on StatusContext {
+                createdAt
+                context
+                description
+                state
+                targetUrl
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+module.exports = async (ref, { context, github }) => {
+  // 誤って ref で取得しようとしてみる
+  {
+    const res = await github.graphql(query, { owner: context.github.owner, repo: context.github.repo, expression: context.github.ref })
+    console.dir(JSON.stringify(res, null, "\t"))
+  }
+
+  // commit ref をちゃんと指定したもの
+  {
+    const res = await github.graphql(query, { owner: context.github.owner, repo: context.github.repo, expression: ref })
+    console.dir(JSON.stringify(res, null, "\t"))
+  }
+
+  return
 }
